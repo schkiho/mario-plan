f0f0260c6aa51308c21d3479eeab704c9f080c4a
schkiho <schkiho@yahoo.de>
2020-02-24 15:02:10 +0100

    setup project summary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
   return (
     <div className='project-list section'>
-      <div className='card z-depth-0 project-summary'>
-        <div card-content className='grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted By Daniel Schmidt</p>
-          <p className='grey-text'>25th February, 5pm </p>
-        </div>
-      </div>
-
-      <div className='card z-depth-0 project-summary'>
-        <div card-content className='grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted By Daniel Schmidt</p>
-          <p className='grey-text'>25th February, 5pm </p>
-        </div>
-      </div>
-
-      <div className='card z-depth-0 project-summary'>
-        <div card-content className='grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted By Daniel Schmidt</p>
-          <p className='grey-text'>25th February, 5pm </p>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
