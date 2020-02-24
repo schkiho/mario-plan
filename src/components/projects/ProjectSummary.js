bb33bfdab43249f6cd5366f887e7dc43cfe520fd
schkiho <schkiho@yahoo.de>
2020-02-24 17:27:30 +0100

    setup navlinks add css

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectSummary = () => {
   return (
     <div className='card z-depth-0 project-summary'>
-      <div card-content className='grey-text text-darken-3'>
+      <div className='card-content grey-text text-darken-3'>
         <span className='card-title'>Project Title</span>
         <p>Posted By Daniel Schmidt</p>
         <p className='grey-text'>25th February, 5pm </p>
