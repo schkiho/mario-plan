43ace1821f6736a383c8d6ba94dcd3da337df935
schkiho <schkiho@yahoo.de>
2020-03-21 19:36:16 +0100

    another cleanup

---
+++ dist/precache-manifest.1799bc7a87108f329b4441409a4d41f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47403fdb0bd276ebf7b8678cc85c54f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164c4d7e849014717148",
+    "url": "/static/css/main.d3282694.chunk.css"
+  },
+  {
+    "revision": "d6c74b5e3753e65df117",
+    "url": "/static/js/2.caf13e49.chunk.js"
+  },
+  {
+    "revision": "4691121c96f91c7f8618b1ecc9480cfc",
+    "url": "/static/js/2.caf13e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164c4d7e849014717148",
+    "url": "/static/js/main.9f72f9e4.chunk.js"
+  },
+  {
+    "revision": "c80bae48cdf384b85e9c",
+    "url": "/static/js/runtime-main.7551f220.js"
+  }
+]);
